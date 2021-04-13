f9f108dd2a5430c13626d340a6360fdbbb6d97d4
soonniee <ckstns1231@gmail.com>
2021-04-14 01:45:59 +0900

    Create User Field

--- users/models.py
+++ users/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
+    avatar = models.ImageField(null=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True)
+    bio = models.TextField(default="")
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, null=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, null=True)
+    superhost = models.BooleanField(default=False)
