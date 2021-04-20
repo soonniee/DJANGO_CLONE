21d01871294d0ca14fc323ab5c4185023d0aba5f
soonniee <ckstns1231@gmail.com>
2021-04-21 00:22:00 +0900

    Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,6 +6,14 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
     """Item Admin Definition"""
 
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -13,7 +21,70 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """Room Admin Definition"""
 
-    pass
+    fielsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")},
+        ),
+        (
+            "Spaces",
+            {
+                "fields": (
+                    "amenities",
+                    "facilties",
+                    "house_rules",
+                )
+            },
+        ),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("guests", "beds", "bedrooms", "baths"),
+            },
+        ),
+        (
+            "Last Details",
+            {"fields": ("host")},
+        ),
+    )
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_photos",
+    )
+    list_filter = (
+        "instant_book",
+        "room_type",
+        "amenities",
+        "facilties",
+        "house_rules",
+        "country",
+        "city",
+    )
+    search_fields = ("=city", "^host__username")
+    filter_horizontal = (
+        "amenities",
+        "facilties",
+        "house_rules",
+    )
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
